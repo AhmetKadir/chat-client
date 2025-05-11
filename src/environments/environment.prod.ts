569383b3303716306ddb5b518fde4d52b3863543
AhmetKadir <ahmetkadir518@gmail.com>
2025-05-11 15:32:44 +0300

    websocket url change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  server: process.env['BACKEND_URL'],
-  webSocket: `${process.env['BACKEND_URL']?.replace('http', 'ws')}/socket`
+  server: 'http://34.105.61.164:8000', // Hardcode or replace during build
+  webSocket: 'ws://34.105.61.164:8080/socket'
+  // server: process.env['BACKEND_URL'],
+  // webSocket: `${process.env['BACKEND_URL']?.replace('http', 'ws')}/socket`
 };
