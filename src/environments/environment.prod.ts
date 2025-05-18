70fd2b43a2793aab3af14ace7c519847845b770c
Ahmet Kadir Aksu <72475558+AhmetKadir@users.noreply.github.com>
2025-05-18 17:22:15 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   server: 'https://chat-api-292563502323.us-central1.run.app', // Hardcode or replace during build
-  webSocket: 'ws://chat-api-292563502323.us-central1.run.app/socket'
+  webSocket: 'wss://chat-api-292563502323.us-central1.run.app/socket'
   // server: process.env['BACKEND_URL'],
   // webSocket: `${process.env['BACKEND_URL']?.replace('http', 'ws')}/socket`
 };
